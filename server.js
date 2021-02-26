280486bf1048d7f353045d8a4e1f2ec3f05dbf88
jodyson24 <joshua.adegbola20@gmail.com>
2021-02-26 12:58:39 +0100

    Update server.js
    Commented out stuffs in the server.js file

--- server.js
+++ server.js
@@ -54,12 +54,12 @@ app.use('/api', require('./routes/notiRouter'))
 app.use('/api', require('./routes/subscribeRouter'))
 
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'))
-    app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
-    })
-}
+//if (process.env.NODE_ENV === 'production') {
+  //  app.use(express.static('client/build'))
+   // app.get('*', (req, res) => {
+       // res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+    //})
+//}
 
 app.listen(5000, () => {
     console.log("Server started at Port", 5000)
