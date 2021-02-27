3bf0204f51074e945fa169b48318ab62e24bf6c6
jodyson24 <joshua.adegbola20@gmail.com>
2021-02-27 06:27:35 +0100

    Update server.js
    Changed some files

--- server.js
+++ server.js
@@ -53,14 +53,15 @@ app.use('/api', require('./routes/categoryRouter'))
 app.use('/api', require('./routes/notiRouter'))
 app.use('/api', require('./routes/subscribeRouter'))
 
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('client/build'))
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+    })
+}
 
-//if (process.env.NODE_ENV === 'production') {
-  //  app.use(express.static('client/build'))
-   // app.get('*', (req, res) => {
-       // res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
-    //})
-//}
+const PORT = process.env.PORT || 7000
 
-app.listen(5000, () => {
-    console.log("Server started at Port", 5000)
+app.listen(PORT, () => {
+    console.log("Server started at Port", PORT)
 })
